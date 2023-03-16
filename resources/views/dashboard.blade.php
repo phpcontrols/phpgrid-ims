c7231603f5385a5d0af58e8ba4178cb62e356c4c
PHPControls <richard@phpcontrols.com>
2023-03-15 22:36:37 -0700

    inventory dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -92,50 +92,109 @@
       </div>
     </div>
   </div>
+
   <div class="row mt-4">
-    <div class="col-lg-7 mb-lg-0 mb-4">
-      <div class="card">
-        <div class="card-body p-3">
-          <div class="row">
-            <div class="col-lg-6">
-              <div class="d-flex flex-column h-100">
-                <p class="mb-1 pt-2 text-bold">Built by developers</p>
-                <h5 class="font-weight-bolder">Soft UI Dashboard</h5>
-                <p class="mb-5">From colors, cards, typography to complex elements, you will find the full documentation.</p>
-                <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
-                  Read More
-                  <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
-                </a>
-              </div>
-            </div>
-            <div class="col-lg-5 ms-auto text-center mt-5 mt-lg-0">
-              <div class="bg-gradient-primary border-radius-lg h-100">
-                <img src="../assets/img/shapes/waves-white.svg" class="position-absolute h-100 w-50 top-0 d-lg-block d-none" alt="waves">
-                <div class="position-relative d-flex align-items-center justify-content-center h-100">
-                  <img class="w-100 position-relative z-index-2 pt-4" src="../assets/img/illustrations/rocket-white.png" alt="rocket">
-                </div>
-              </div>
-            </div>
-          </div>
+    <div class="col-lg-12">
+      <div class="card h-100">
+
+        <div class="card-header pb-0">
+            <h6>Current Inventory Summary</h6>
         </div>
-      </div>
-    </div>
-    <div class="col-lg-5">
-      <div class="card h-100 p-3">
-        <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100" style="background-image: url('../assets/img/ivancik.jpg');">
-          <span class="mask bg-gradient-dark"></span>
+
+        <div class="overflow-hidden position-relative border-radius-lg bg-cover h-100">
           <div class="card-body position-relative z-index-1 d-flex flex-column h-100 p-3">
-            <h5 class="text-white font-weight-bolder mb-4 pt-2">Work with the rockets</h5>
-            <p class="text-white">Wealth creation is an evolutionarily recent positive-sum game. It is all about who take the opportunity first.</p>
-            <a class="text-white text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="javascript:;">
-              Read More
-              <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
-            </a>
-          </div>
+
+            <div class="row">
+            
+              <div class="col-lg-7">
+
+                <?php 
+                use phpCtrl\C_DataGrid;
+
+                $pc = new \C_PhpChartX(array(array(null)), 'PieChart');
+                //$pc->set_title(array('text'=>'Current Inventory Summary'));
+                $pc->set_series_default(array( 'shadow'=> false, 
+                    'renderer'=> 'plugin::PieRenderer', 
+                    'rendererOptions'=> array( 
+                      'showDataLabels' => true,
+                      'highlightMouseOver' => true,
+                      'startAngle'=> 180, 
+                      'sliceMargin'=> 4, 
+                      'showDataLabels'=> true ) 
+                  ));
+                $pc->set_legend(array('show'=>true,'location'=> 'w'));
+                // remove grids.
+                $pc->set_grid(array(
+                    'background'=>'white',
+                    'borderWidth'=>0,
+                    'borderColor'=>'#000000',
+                    'shadow'=>false));
+                $pc->add_custom_js("
+                        $('#PieChart').bind('jqplotDataHighlight',
+                            function (ev, seriesIndex, pointIndex, data) {
+                              $('#label_info').text(data);      
+                            }
+                        );
+                    ");
+                $pc->draw(660,400);  
+                ?>
+
+              </div>
+            
+              <div class="col-lg-5">
+
+                <?php
+                $dgProd = new C_DataGrid('SELECT id, ProductLabel, InventoryReceived FROM products', 'id', 'products');
+                $dgProd->set_col_hidden('id', false);
+                $dgProd->set_dimension('auto', 'auto')->set_pagesize(100);
+                $onGridLoadComplete = <<<ONGRIDLOADCOMPLETE
+                function(status, rowid)
+                {
+                  var dataX = [];
+                  var dataY = [];
+
+                  d1 = $('#products').jqGrid('getCol', 'ProductLabel', false);
+                  d2 = $('#products').jqGrid('getCol', 'InventoryReceived', false);
+                  
+                  npoints = d1.length;
+                  for(var i=0; i < npoints; i++){
+                    dataX[i] = [i+1, d1[i]];
+                    dataY[i] = [i+1, parseInt(d2[i])];
+                  }
+
+                    var pieData = [];
+                    for(var j=0; j < dataX.length; j++)
+                    {
+                        pieData.push([dataX[j][1], dataY[j][1]]);
+                    }
+                    console.log(pieData);
+                    _PieChart.series[0].data = pieData;
+                    _PieChart.replot({resetAxes:true});
+                }
+                ONGRIDLOADCOMPLETE;
+                $dgProd->add_event("jqGridLoadComplete", $onGridLoadComplete);
+                $dgProd->set_dimension('500px');
+                $dgProd->display();
+                ?>
+
+                <style>
+                div#resizable {
+                  padding-top:100px
+                }
+                div#gbox_products {
+                  margin-top:20px;
+                }
+                </style>
+
+              </div>
+
+            </div>
+
         </div>
       </div>
     </div>
   </div>
+
   <div class="row mt-4">
     <div class="col-lg-5 mb-lg-0 mb-4">
       <div class="card z-index-2">
