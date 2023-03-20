fa9633c6efcb28b24238c5b50d1c3fd5d402c485
PHPControls <richard@phpcontrols.com>
2023-03-19 21:49:10 -0700

    chagne user profile to display actual user name

--- resources/views/laravel-examples/user-profile.blade.php
+++ resources/views/laravel-examples/user-profile.blade.php
@@ -20,7 +20,7 @@
                 <div class="col-auto my-auto">
                     <div class="h-100">
                         <h5 class="mb-1">
-                            {{ __('Alec Thompson') }}
+                            {{ auth()->user()->name }}
                         </h5>
                         <p class="mb-0 font-weight-bold text-sm">
                             {{ __(' CEO / Co-Founder') }}
