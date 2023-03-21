8557c099b05e24362a125f9882e9033baf5eeaea
PHPControls <richard@phpcontrols.com>
2023-03-20 19:01:31 -0700

    side bar links to lookup table admin page

--- resources/views/layouts/navbars/auth/sidebar.blade.php
+++ resources/views/layouts/navbars/auth/sidebar.blade.php
@@ -113,7 +113,7 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Tables</span>
+          <span class="nav-link-text ms-1">Supplier Management</span>
         </a>
       </li>
       <li class="nav-item">
@@ -133,7 +133,7 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Billing</span>
+          <span class="nav-link-text ms-1">Category Management</span>
         </a>
       </li>
       <li class="nav-item">
@@ -154,7 +154,7 @@
               </g>
             </svg>
           </div>
-          <span class="nav-link-text ms-1">Virtual Reality</span>
+          <span class="nav-link-text ms-1">Tags Management</span>
         </a>
       </li>
       <li class="nav-item">
