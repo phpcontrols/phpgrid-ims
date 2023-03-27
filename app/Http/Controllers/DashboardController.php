ff2ef6b482592e7bf0b64fcaca1644fd90d32349
PHPControls <richard@phpcontrols.com>
2023-03-26 23:14:44 -0700

    Dash board mostOrderedProduct

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -17,6 +17,37 @@ class DashboardController extends Controller
         $ordersShipped = DB::connection('im')->table('orders')
                 ->sum('NumberShipped');
 
-        return view('dashboard')->with(['ordersShipped'=>$ordersShipped]);
+
+        /*
+        // SELECT 
+        *
+        FROM orders
+        WHERE NumberShipped = 
+            (select max(NumberShipped) from orders)
+        */
+
+        /*
+        DB::table('orders')
+            ->select('products.ProductName')
+            ->join('products','orders.productId','=','products.id')
+            ->where('NumberShipped','=',function($query) {
+                $query->from('orders')
+                    ->select(DB::raw("'max'(NumberShipped));
+            })
+            ->get();
+        */
+
+        // converted with /sql-to-laravel-builder/, https://sql2builder.github.io/
+        // return scalar value 
+        $mostOrderedProduct = DB::connection('im')->table('orders')
+                    ->join('products','orders.productId','=','products.id')
+                    ->whereRaw('NumberShipped = (select max(NumberShipped) from orders)')
+                    ->value('products.ProductName');
+
+
+
+        return view('dashboard')->with(['ordersShipped' => $ordersShipped,
+                                        'mostOrderedProduct' => $mostOrderedProduct
+                                        ]);
     }
 }
