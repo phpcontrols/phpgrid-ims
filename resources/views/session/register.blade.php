40620b6036517c2c09850a75f0169995f8758074
PHPControls <richard@phpcontrols.com>
2023-04-17 21:50:52 -0700

    replace default background

--- resources/views/session/register.blade.php
+++ resources/views/session/register.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
   <section class="min-vh-100 mb-8">
-    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 mx-3 border-radius-lg" style="background-image: url('../assets/img/bokeh/pexels-bokeh2.jpeg');">
+    <div class="page-header align-items-start min-vh-50 pt-5 pb-11 mx-3 border-radius-lg" style="background-image: url('../assets/img/inventory1.jpeg');">
       <span class="mask bg-gradient-dark opacity-6"></span>
       <div class="container">
         <div class="row justify-content-center">
